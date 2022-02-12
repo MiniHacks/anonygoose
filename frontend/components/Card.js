a9ee53ddd4c668cdce65b27d02da7976358b41e9
iCalculated <fufa0001@gmail.com>
2022-02-12 16:44:44 -0600

    filler text and border removal

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -3,7 +3,7 @@ import { SettingsIcon } from '@chakra-ui/icons'
 
 export default function CustomCard({title, description}) {
   return (
-    <Box maxW='sm' borderWidth='1px' overflow='auto'>
+    <Box maxW='sm' overflow='auto'>
       <Heading as='h3' size='md'> 
         <SettingsIcon color='pink.500'/>
         {title}
--- frontend/components/FeatureGrid.js
+++ frontend/components/FeatureGrid.js
@@ -1,15 +1,13 @@
-import {Grid, GridItem } from '@chakra-ui/react'
+import { SimpleGrid, GridItem } from '@chakra-ui/react'
 import Card from "./Card.js";
 
 export default function FeatureGrid() {
   return (
-    <Grid templateColumns='repeat(2, 1fr)' gap={4}>
-      <GridItem w='100%' h='10' bg='blue.500'>
-        <Card title="Test" description="test?"/>
-      </GridItem>
-      <GridItem w='100%' h='10' bg='blue.500'/>
-      <GridItem w='100%' h='10' bg='blue.500'/>
-      <GridItem w='100%' bg='blue.500'/>
-    </Grid>
+    <SimpleGrid columns={2} spacing={4}>
+      <Card title="Privacy" description="ML blurs faces which makes everything private. Wow."/>
+      <Card title="No faces" description="Don't have to see people. Wow this is good text.?"/>
+      <Card title="Good" description="Good? Good. Good! Probably."/> 
+      <Card title="Socially ethical" description="I hope the judges are compelled by how ethical this is."/>
+    </SimpleGrid>
   )
 }
