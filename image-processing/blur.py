56dd9994d0fdd11b969135cce1d0f5f3d31005e2
Jack Lee <welsar55@gmail.com>
2022-02-12 17:07:23 -0600

    Find face embeddings

--- image-processing/blur.py
+++ image-processing/blur.py
@@ -1,46 +1,114 @@
 import imutils
 import cv2
 import numpy as np
+import os
 
 THRESHOLD = 0.6
 KERNEL = (30,30)
+MARGIN = 0.25
+MINIMUM_SIZE = 20
 
 def setup_network(prototxt, model):
     return cv2.dnn.readNetFromCaffe(prototxt, model)
 
-net = setup_network("./image-processing/deploy.prototxt", "./image-processing/res10_300x300_ssd_iter_140000.caffemodel")
+def setup_embedder(model):
+    return cv2.dnn.readNetFromTorch(model)
 
-def blur(image, userid):
+net = setup_network("./image-processing/deploy.prototxt", "./image-processing/res10_300x300_ssd_iter_140000.caffemodel")
+embedder = setup_embedder("./image-processing/openface_nn4.small2.v1.t7")
+
+def get_face_embeddings(userid):
+    pass # set this up to get faces from the db.
+
+def train_to_ignore(training_sets):
+    to_ret = []
+    for set in training_sets:
+        embeddings = []
+
+        i = 0
+        for image in set:
+            print("Vectorizing image", i, "...")
+            i += 1
+
+            image = imutils.resize(image, width=600)
+
+            regions = find_faces(image, None)
+            for (startX, startY, endX, endY) in regions:
+
+                if endY - startY < MINIMUM_SIZE or endX - startX < MINIMUM_SIZE:
+                    continue
+
+                face = image[startY:endY, startY:endY]
+                
+                faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,(96, 96), (0, 0, 0), swapRB=True, crop=False)
+                embedder.setInput(faceBlob)
+                face_vec = embedder.forward()
+                embeddings.append(face_vec.flatten())
+                print("Face vectorized for image", i, ":)")
+
+                # Assume only one face per image.
+                break
+        
+        to_ret.append(embeddings)
+    return to_ret
+
+
+def find_faces(image, to_ignore):
     (h, w) = image.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
 
     net.setInput(blob)
     detections = net.forward()
-    n = 0
+    regions = []
 
     for i in range(0, detections.shape[2]):
-
         confidence = detections[0, 0, i, 2]
         if confidence > THRESHOLD:
-            n += 1
-
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+            regions.append(box.astype("int"))
 
-            blurred_part = cv2.blur(image[startY:endY, startX:endX], ksize=KERNEL)
-            image[startY:endY, startX:endX] = blurred_part
+    return regions
 
-            text = "{:.2f}%".format(confidence * 100) + 'Count ' + str(n)
-            y = startY - 10 if startY - 10 > 10 else startY + 10
-            cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
-            cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+def blur(image, regions):
+    (h, w) = image.shape[:2]
+    to_ret = image.copy()
 
-    cv2.imshow("preview", image)
+    for (startX, startY, endX, endY) in regions:
+
+        yMargin = (MARGIN * (endY - startY)) / 2.0
+        xMargin = (MARGIN * (endX - startX)) / 2.0
+        startY = int(startY - yMargin)
+        endY = int(endY + yMargin)
+        startX = int(startX - xMargin)
+        endX = int(endX + xMargin)
+
+        startY = 0 if startY < 0 else startY
+        endY = h if endY > h else endY
+        startX = 0 if startX < 0 else startX
+        endX = w if endX > w else endX
+
+        blurred_part = cv2.blur(to_ret[startY:endY, startX:endX], ksize=KERNEL)
+        to_ret[startY:endY, startX:endX] = blurred_part
+
+        cv2.rectangle(to_ret, (startX, startY), (endX, endY), (0, 0, 255), 2)
+
+    cv2.imshow("preview", to_ret)
     cv2.waitKey(0)
 
+    return to_ret
+
 def test():
-    image = cv2.imread("C:/Users/welsa/Pictures/manicare-blog-face-shapes-inlaid-2.jpg")
-    blur(image, None)
+    image = cv2.imread("C:/Users/welsa/Pictures/WIN_20220212_16_20_48_Pro.jpg")
+
+    to_train = []
+    folder_path = "C:/Users/welsa/Pictures/test-recognition"
+    for filename in os.listdir(folder_path):
+        f = os.path.join(folder_path, filename)
+        to_train.append(cv2.imread(f))
+
+    models = train_to_ignore([to_train])
+
+    blur(image, find_faces(image, None))
 
 if __name__ == "__main__":
     test()
