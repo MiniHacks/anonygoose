f8356e7beea9a0906aa8bc30f4f12ef91b845449
Samyok Nepal <samyok@fiveable.me>
2022-02-13 11:12:49 -0600

    lots of shit

--- frontend/pages/setup.js
+++ frontend/pages/setup.js
@@ -4,19 +4,21 @@ import {PageLayout} from "../components/PageLayout";
 import Layout from "../components/Layout";
 import MyButton from "../components/MyButton";
 import dynamic from "next/dynamic";
-const FileUpload = dynamic(() => import("../components/FileUpload"), {ssr: false})
 import {useRouter} from "next/router";
 import {useCallback, useEffect, useState} from "react";
+
+const FileUpload = dynamic(() => import("../components/FileUpload"), {ssr: false})
 
 export default function Home() {
     const [youtubeIngestionUrl, setYoutubeIngestionUrl] = useState('')
     const [youtubeStreamName, setYoutubeStreamName] = useState('')
-  
+    const [showDropZone, setShowDropZone] = useState(false);
     const [streamId, setStreamId] = useState('')
     const [broadcastId, setBroadcastId] = useState('')
     const {data: session, status} = useSession()
     const router = useRouter();
     const [rtmp, setRtmp] = useState("");
+    const [images, setImages] = useState([])
     const toast = useToast()
 
 
@@ -26,6 +28,9 @@ export default function Home() {
         } else if (status === "authenticated") {
             fetch("/api/livestream/start").then(r => r.json()).then(r => {
                 setRtmp(r.rtmp)
+            })
+            fetch("/api/images").then(r => r.json()).then(r => {
+                setImages(r.map(i => i.path))
             })
         }
     }, [status])
@@ -42,10 +47,10 @@ export default function Home() {
 
     const HELLLLLL = useCallback(async () => {
         console.log("fuck")
-        if (!session) return; 
+        if (!session) return;
         console.log("me")
         const response = await fetch("/api/livestream/start")
-        const data = await response.json() 
+        const data = await response.json()
         const access_token = data.account.access_token;
         console.log("ACCESS TOKEN", access_token);
         gapi.load("client:auth2", function () {
@@ -108,84 +113,84 @@ export default function Home() {
 
     const bindBroadcastToStream = (id) => {
         return gapi.client.youtube.liveBroadcasts
-          .bind({
-            part: ['id,snippet,contentDetails,status'],
-            id: id,
-            streamId: streamId,
-          })
-          .then((res) => {
-            //console.log('Response', res)
-            console.log(res.result.id)
-          })
-          .catch((err) => {
-            console.error('Execute error', err)
-          })
+            .bind({
+                part: ['id,snippet,contentDetails,status'],
+                id: id,
+                streamId: streamId,
+            })
+            .then((res) => {
+                //console.log('Response', res)
+                console.log(res.result.id)
+            })
+            .catch((err) => {
+                console.error('Execute error', err)
+            })
     }
 
     const createStream = () => {
         return gapi.client.youtube.liveStreams
-          .insert({
-            part: ['snippet,cdn,contentDetails,status'],
-            resource: {
-              snippet: {
-                title: "Your new video stream's name",
-                description:
-                  'A description of your video stream. This field is optional.',
-              },
-              cdn: {
-                frameRate: 'variable',
-                ingestionType: 'rtmp',
-                resolution: 'variable',
-                format: '',
-              },
-              contentDetails: {
-                isReusable: true,                                        
-                enableAutoStart: true,
-                broadcastType: "persistent"
-              },
-            },
-          })
-          .then((res) => {
-            //console.log('Response', res)
-    
-            setStreamId(res.result.id)
-            console.log('streamID' + res.result.id)
-    
-            setYoutubeIngestionUrl(res.result.cdn.ingestionInfo.ingestionAddress)
-            console.log(res.result.cdn.ingestionInfo.ingestionAddress)
-    
-            setYoutubeStreamName(res.result.cdn.ingestionInfo.streamName)
-            console.log(res.result.cdn.ingestionInfo.streamName)
-          })
-          .catch((err) => {
-            console.log('Execute error', err)
-          })
+            .insert({
+                part: ['snippet,cdn,contentDetails,status'],
+                resource: {
+                    snippet: {
+                        title: "Your new video stream's name",
+                        description:
+                            'A description of your video stream. This field is optional.',
+                    },
+                    cdn: {
+                        frameRate: 'variable',
+                        ingestionType: 'rtmp',
+                        resolution: 'variable',
+                        format: '',
+                    },
+                    contentDetails: {
+                        isReusable: true,
+                        enableAutoStart: true,
+                        broadcastType: "persistent"
+                    },
+                },
+            })
+            .then((res) => {
+                //console.log('Response', res)
+
+                setStreamId(res.result.id)
+                console.log('streamID' + res.result.id)
+
+                setYoutubeIngestionUrl(res.result.cdn.ingestionInfo.ingestionAddress)
+                console.log(res.result.cdn.ingestionInfo.ingestionAddress)
+
+                setYoutubeStreamName(res.result.cdn.ingestionInfo.streamName)
+                console.log(res.result.cdn.ingestionInfo.streamName)
+            })
+            .catch((err) => {
+                console.log('Execute error', err)
+            })
     }
 
     const listLivestreams = async () => {
-        return gapi.client.youtube.liveStreams.list( {
-                part: ['snippet,id,cdn,status'],
-                mine: true
-            })
+        return gapi.client.youtube.liveStreams.list({
+            part: ['snippet,id,cdn,status'],
+            mine: true
+        })
     }
 
     const transitionToLive = (id) => {
         return gapi.client.youtube.liveBroadcasts
             .transition({
-            part: ['id,snippet,contentDetails,status'],
-            broadcastStatus: 'live',
-            id: id,
+                part: ['id,snippet,contentDetails,status'],
+                broadcastStatus: 'live',
+                id: id,
             })
             .then((res) => {
-            // Handle the results here (response.result has the parsed body).
-            console.log('Response', res)
+                // Handle the results here (response.result has the parsed body).
+                console.log('Response', res)
             })
             .catch((err) => {
-            console.log('Execute error', err)
+                console.log('Execute error', err)
             })
     }
 
-    return (<PageLayout wave={'40vh'}>
+    return (<PageLayout wave={'50vh'}>
         <Layout pt={'13vh'}>
             <Flex justifyContent={"space-between"} alignItems={'center'} flexWrap={"wrap"}>
                 <Box>
@@ -222,13 +227,35 @@ export default function Home() {
             <Flex justifyContent={"space-between"} alignItems={'center'} flexWrap={"wrap"}>
                 <Box>
                     <Heading size={"2xl"} color={"#0A2540"} lineHeight={1.3}>
-                        Your Team
+                        Your Team <MyButton onClick={() => setShowDropZone(pv => !pv)}>+</MyButton>
                     </Heading>
-                    <Text color={'gray.600'}>
+                    <Text color={'gray.600'} mb={3}>
                         Submit pictures of people to whitelist.
                     </Text>
-                    <FileUpload onFileAccepted={data => console.log(data)}/>
-
+                    {showDropZone && <FileUpload onFileAccepted={data => {
+                        const uploadPictureHandler = async (file) => {
+                            const pictureData = new FormData();
+                            pictureData.append('image', file);
+                            try {
+                                const response = await fetch('/api/upload', {
+                                    method: 'POST',
+                                    body: pictureData,
+                                });
+                                const data = await response.json();
+                                if (!response.ok) {
+                                    throw data;
+                                }
+                                console.log(data);
+                                router.reload();
+                            } catch (error) {
+                                console.log(error.message);
+                            }
+                        };
+                        uploadPictureHandler(data[0])
+                    }}/>}
+                    <HStack spacing={4} mt={4}>
+                        {images.map(i => <chakra.img borderRadius={"sm"} width={250} src={process.env.NEXT_PUBLIC_IMG_URL+i} key={i}/>)}
+                    </HStack>
                 </Box>
             </Flex>
             <Flex justifyContent={"space-between"} alignItems={'center'} flexWrap={"wrap"} mt={40}>
